78aac69f0504f5a42f5c57c9fc9af57e8dd7ea75
Peter Dinis <peterdinis0@pobox.sk>
2024-06-14 14:55:15 +0200

    feat: FOO

--- app/component/home/LandingWrapper.tsx
+++ app/component/home/LandingWrapper.tsx
@@ -8,14 +8,12 @@ import {
   Heading,
   Text,
   Button,
-  Image,
-  Icon,
   IconButton,
-  createIcon,
-  IconProps,
   useColorModeValue,
 } from '@chakra-ui/react'
+import Link from 'next/link'
 import { FC } from 'react'
+import { Blob } from '../shared/Blob'
 
 const LandingWrapper: FC = () => {
   return (
@@ -50,10 +48,8 @@ const LandingWrapper: FC = () => {
               Prep
             </Text>
           </Heading>
-          <Text color={'gray.500'}>
-            Snippy is a rich coding snippets app that lets you create your own code
-            snippets, categorize them, and even sync them in the cloud so you can use them
-            anywhere. All that is free!
+          <Text color={'white.500'} fontSize={"1.2rem"} letterSpacing={"-1%"} fontWeight={"bold"}>
+          Are you ready to ace your next job interview? Look no further than InterviewPrep, the ultimate platform designed to help you master the art of interviewing. Whether you're a recent graduate, a seasoned professional, or someone looking to make a career change, our app offers tailored mock interviews to prepare you for every question and scenario.
           </Text>
           <Stack spacing={{ base: 4, sm: 6 }} direction={{ base: 'column', sm: 'row' }}>
             <Button
@@ -64,14 +60,16 @@ const LandingWrapper: FC = () => {
               colorScheme={'red'}
               bg={'red.400'}
               _hover={{ bg: 'red.500' }}>
+              <Link href="/register">
               Get started
+              </Link>
             </Button>
             <Button
               rounded={'full'}
               size={'lg'}
               fontWeight={'normal'}
               px={6}>
-              How It Works
+              About Interview Prep
             </Button>
           </Stack>
         </Stack>
@@ -90,6 +88,7 @@ const LandingWrapper: FC = () => {
             zIndex={-1}
             color={useColorModeValue('red.50', 'red.400')}
           />
+          
           <Box
             position={'relative'}
             height={'300px'}
@@ -115,21 +114,3 @@ const LandingWrapper: FC = () => {
 }
 
 export default LandingWrapper;
-
-const Blob = (props: IconProps) => {
-  return (
-    <Icon
-      width={'100%'}
-      viewBox="0 0 578 440"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}>
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z"
-        fill="currentColor"
-      />
-    </Icon>
-  )
-}
