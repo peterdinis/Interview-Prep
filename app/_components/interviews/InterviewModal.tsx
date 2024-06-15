3b89e21ce504f4932cb8ca0b5391028df828c9e2
Peter Dinis <peterdinis0@pobox.sk>
2024-06-15 15:42:27 +0200

    feat: Update interview modal

--- app/_components/interviews/InterviewModal.tsx
+++ app/_components/interviews/InterviewModal.tsx
@@ -11,6 +11,8 @@ import {
     ModalBody,
     ModalFooter,
     Text,
+    Input,
+    Stack,
 } from '@chakra-ui/react';
 import { FC } from 'react';
 
@@ -19,20 +21,58 @@ const InterviewModal: FC = () => {
 
     return (
         <>
-            <Text onClick={onOpen}>Open Modal</Text>
+            <Text onClick={onOpen}>New Interview</Text>
 
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader display={}>Modal Title</ModalHeader>
+                    <ModalHeader
+                        display={'flex'}
+                        justifyContent={'center'}
+                        fontSize={'2rem'}
+                        mt={5}
+                        alignItems={'center'}
+                    >
+                        New Interview
+                    </ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody>rrrrrrrrrr</ModalBody>
+                    <ModalBody>
+                        <Text fontWeight={"bold"} color="red.600" p={1}>Tell me something about your job</Text>
+                        <Stack mt={5} spacing={3}>
+                            <form>
+                                <Text mt={3} fontWeight={'bold'}>
+                                    Your Job
+                                </Text>
+                                <Input mt={2} placeholder='Here is a sample placeholder' />
+
+                                <Text mt={3} fontWeight={'bold'}>
+                                    Your Job
+                                </Text>
+                                <Input mt={2} placeholder='Here is a sample placeholder' />
+
+                                <Text mt={3} fontWeight={'bold'}>
+                                    Your Job
+                                </Text>
+                                <Input mt={2} placeholder='Here is a sample placeholder' />
+
+                                <Text mt={3} fontWeight={'bold'}>
+                                    Your Job
+                                </Text>
+                                <Input mt={2} placeholder='Here is a sample placeholder' />
+
+                                <Text mt={3} fontWeight={'bold'}>
+                                    Your Job
+                                </Text>
+                                <Input mt={2} placeholder='Here is a sample placeholder' />
+                            </form>
+                        </Stack>
+                    </ModalBody>
 
                     <ModalFooter>
                         <Button colorScheme='blue' mr={3} onClick={onClose}>
                             Close
                         </Button>
-                        <Button variant='ghost'>Secondary Action</Button>
+                        <Button variant='ghost'>Generate</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
