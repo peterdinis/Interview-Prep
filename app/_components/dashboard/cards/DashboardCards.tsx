b10108727589eadeeef1b2cf34aa198aaaa48da7
Peter Dinis <peterdinis0@pobox.sk>
2024-06-15 16:46:24 +0200

    FOO

--- app/_components/dashboard/cards/DashboardCards.tsx
+++ app/_components/dashboard/cards/DashboardCards.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import { FC } from 'react';
 import { SimpleGrid, Container } from '@chakra-ui/react';
--- app/_components/shared/Lazy-Image.tsx
+++ app/_components/shared/Lazy-Image.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import * as React from 'react';
 import { Image } from '@chakra-ui/react';
