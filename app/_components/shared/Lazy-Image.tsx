4b31449b32181a6368c63a3dcc417e64588e76ad
Peter Dinis <peterdinis0@pobox.sk>
2024-06-15 16:18:49 +0200

    FOO

--- app/_components/shared/Lazy-Image.tsx
+++ app/_components/shared/Lazy-Image.tsx
@@ -2,27 +2,27 @@ import * as React from 'react';
 import { Image } from '@chakra-ui/react';
 
 type LazyImageProps = {
-  src: string;
-  size?: string;
-  width?: number;
-  height?: number;
-  layout?: string;
-  rounded?: string;
+    src: string;
+    size?: string;
+    width?: number;
+    height?: number;
+    layout?: string;
+    rounded?: string;
 };
 
 const LazyImage = (props: LazyImageProps) => {
-  const { src, width, height, rounded } = props;
+    const { src, width, height, rounded } = props;
 
-  return (
-    <Image
-      src={src}
-      objectFit="cover"
-      alt="cover image"
-      width={width}
-      height={height}
-      rounded={rounded}
-    />
-  );
+    return (
+        <Image
+            src={src}
+            objectFit='cover'
+            alt='cover image'
+            width={width}
+            height={height}
+            rounded={rounded}
+        />
+    );
 };
 
 export default LazyImage;
