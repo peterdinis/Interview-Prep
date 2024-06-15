87865201228916e5b1fbe95e9d5ff8cb6b291ff7
Peter Dinis <peterdinis0@pobox.sk>
2024-06-15 19:24:27 +0200

    FOO

--- database/connect.ts
+++ database/connect.ts
@@ -2,8 +2,8 @@ import { mongoMigrateCli } from 'mongo-migrate-ts';
 import { db } from './db';
 
 mongoMigrateCli({
-  uri: process.env.DATABASE_URL!,
-  database: "interviewprep",
-  migrationsDir: __dirname,
-  migrationsCollection: 'migrations',
+    uri: process.env.DATABASE_URL!,
+    database: 'interviewprep',
+    migrationsDir: __dirname,
+    migrationsCollection: 'migrations',
 });
