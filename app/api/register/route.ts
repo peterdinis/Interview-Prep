a452b406c785b53f56ea96714eb4dd310584f8c2
Peter Dinis <peterdinis0@pobox.sk>
2024-06-17 17:49:54 +0200

    feat: Update endpoint

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,12 +1,9 @@
-import { connectDB } from '@/libs/mongodb';
-import User from '@/models/User';
+import { db } from '@/database/db';
 import { NextResponse } from 'next/server';
-import bcrypt from 'bcryptjs';
-import mongoose from 'mongoose';
+import * as bcrypt from "bcrypt";
 
 export async function POST(request: Request) {
     try {
-        await connectDB();
 
         const { name, email, password, phone } = await request.json();
 
@@ -17,7 +14,9 @@ export async function POST(request: Request) {
             );
         }
 
-        const userFound = await User.findOne({ email });
+        const userFound = await db.user.findFirst({ 
+            where: email
+         });
 
         if (userFound) {
             return NextResponse.json(
@@ -28,26 +27,25 @@ export async function POST(request: Request) {
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
-        const user = new User({
-            name,
-            email,
-            phone,
-            password: hashedPassword,
-        });
-
-        const savedUser = await user.save();
+        const user = await db.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        })
 
         return NextResponse.json(
             {
-                name: savedUser.name,
-                email: savedUser.email,
-                createdAt: savedUser.createdAt,
-                updatedAt: savedUser.updatedAt,
+                name: user.name,
+                email: user.email,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt,
             },
             { status: 201 },
         );
-    } catch (error) {
-        if (error instanceof mongoose.Error.ValidationError) {
+    } catch (error: any) {
+        if (error) {
             return NextResponse.json(
                 { message: error.message },
                 { status: 400 },
@@ -56,106 +54,5 @@ export async function POST(request: Request) {
             console.error('Error during signup:', error);
             return NextResponse.error();
         }
-    }
-}
-
-export async function PUT(request: Request) {
-    try {
-        await connectDB();
-
-        const { userId, name, email, password, phone, address } =
-            await request.json();
-
-        if (password && password.length < 6) {
-            return NextResponse.json(
-                { message: 'Password must be at least 6 characters' },
-                { status: 400 },
-            );
-        }
-
-        const userToUpdate = await User.findById(userId);
-
-        if (!userToUpdate) {
-            return NextResponse.json(
-                { message: 'User not found' },
-                { status: 404 },
-            );
-        }
-
-        if (name) {
-            userToUpdate.name = name;
-        }
-
-        if (email) {
-            userToUpdate.email = email;
-        }
-
-        if (password) {
-            const hashedPassword = await bcrypt.hash(password, 12);
-            userToUpdate.password = hashedPassword;
-        }
-
-        if (phone) {
-            userToUpdate.phone = phone;
-        }
-
-        if (address) {
-            userToUpdate.address = address;
-        }
-
-        await userToUpdate.save();
-
-        console.log(userToUpdate);
-
-        return NextResponse.json(
-            {
-                message: 'User updated successfully',
-                updatedUser: {
-                    id: userToUpdate._id,
-                    name: userToUpdate.name,
-                    email: userToUpdate.email,
-                    createdAt: userToUpdate.createdAt,
-                    updatedAt: userToUpdate.updatedAt,
-                },
-            },
-            { status: 200 },
-        );
-    } catch (error) {
-        if (error instanceof mongoose.Error.ValidationError) {
-            return NextResponse.json(
-                { message: error.message },
-                { status: 400 },
-            );
-        } else {
-            console.error('Error during user update:', error);
-            return NextResponse.error();
-        }
-    }
-}
-
-export async function DELETE(request: Request) {
-    try {
-        await connectDB();
-
-        const { userId } = await request.json();
-
-        const user = await User.findById(userId);
-
-        if (!user) {
-            return NextResponse.json(
-                { message: 'User not found' },
-                { status: 404 },
-            );
-        }
-
-        await user.remove();
-
-        return NextResponse.json(
-            { message: 'User deleted successfully' },
-            { status: 200 },
-        );
-    } catch (error) {
-        console.error('Error during user/cart item deletion:', error);
-        return NextResponse.error();
     }
 }
