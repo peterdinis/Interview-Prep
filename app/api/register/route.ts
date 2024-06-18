4bcfa40270f30ddd19ad326e2fa5ea6aa82ed0e6
Peter Dinis <peterdinis0@pobox.sk>
2024-06-18 17:32:50 +0200

    FOO

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,57 +1,34 @@
 import { db } from '@/database/db';
-import { NextResponse } from 'next/server';
-import * as bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(request: Request) {
-    try {
-        const { name, email, password, phone } = await request.json();
+export async function POST(request: NextRequest){
+    const body = await request.json();
+    const { name, email, password } = body;
 
-        if (password.length < 6) {
-            return NextResponse.json(
-                { message: 'Password must be at least 6 characters' },
-                { status: 400 },
-            );
-        }
-
-        const userFound = await db.user.findFirst({
-            where: email,
-        });
-
-        if (userFound) {
-            return NextResponse.json(
-                { message: 'Email already exists' },
-                { status: 409 },
-            );
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 12);
-
-        const user = await db.user.create({
-            data: {
-                name,
-                email,
-                password: hashedPassword,
-            },
-        });
-
-        return NextResponse.json(
-            {
-                name: user.name,
-                email: user.email,
-                createdAt: user.createdAt,
-                updatedAt: user.updatedAt,
-            },
-            { status: 201 },
-        );
-    } catch (error: any) {
-        if (error) {
-            return NextResponse.json(
-                { message: error.message },
-                { status: 400 },
-            );
-        } else {
-            console.error('Error during signup:', error);
-            return NextResponse.error();
-        }
+    if(!name || !email || !password) {
+        return new NextResponse('Missing Fields', { status: 400 })
     }
+
+    const exist = await db.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if(exist) {
+        throw new Error('Email already exists')
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    return NextResponse.json(user)
 }
