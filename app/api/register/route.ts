829b232e925e7057965524469d19e2bf517e06b6
Peter Dinis <peterdinis0@pobox.sk>
2024-06-20 22:06:55 +0200

    FOO

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
-    const { name, email, password } = body;
+    const { email, password } = body;
 
-    if (!name || !email || !password) {
+    if (!email || !password) {
         return new NextResponse('Missing Fields', { status: 400 });
     }
 
@@ -24,7 +24,6 @@ export async function POST(request: NextRequest) {
 
     const user = await db.user.create({
         data: {
-            name,
             email,
             password: hashedPassword,
         },
