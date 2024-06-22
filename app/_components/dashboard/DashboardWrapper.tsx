f178218e87028c4f358c3334fc69283715e7ea4f
Peter Dinis <peterdinis0@pobox.sk>
2024-06-22 22:06:14 +0200

    feat: New not allowed page

--- app/_components/dashboard/DashboardWrapper.tsx
+++ app/_components/dashboard/DashboardWrapper.tsx
@@ -1,14 +1,25 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { AppShell } from '@saas-ui/react';
 import DashboardSidebar from './DashboardSidebar';
 import { Box } from '@chakra-ui/react';
 import DashboardContent from './DashboardContent';
 import DashboardCards from './cards/DashboardCards';
 import SearchInput from '../shared/SearchInput';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const DashboardWrapper: FC = () => {
+    const { data: session } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!session) {
+            router.push('/not-allowed');
+        }
+    }, [session]);
+
     return (
         <AppShell minH='100vh' mt={20} sidebar={<DashboardSidebar />}>
             <Box
---
+++ app/not-allowed/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { Button, Center, Flex, Link, Text } from "@chakra-ui/react";
+import { NextPage } from "next";
+
+const NotAllowedPage: NextPage = () => {
+    return (
+        <>
+            <Flex direction={"column"} justifyContent={"center"} alignItems={"center"}>
+                <Center mt={20}>
+                    <Text fontWeight={"bold"} fontSize={36}>You must be logged in first</Text>
+                </Center>
+                <Button mt={8}>
+                    <Link fontSize={22} href="/login">Login</Link>
+                </Button>
+            </Flex>
+        </>
+    )
+}
+
+export default NotAllowedPage;
