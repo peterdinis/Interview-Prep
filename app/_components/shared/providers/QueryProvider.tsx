0f4ee3713cc76a89eae079b0b32594cd3034452e
Peter Dinis <peterdinis0@pobox.sk>
2024-07-01 15:19:07 +0200

    FOO

--- app/_components/shared/providers/QueryProvider.tsx
+++ app/_components/shared/providers/QueryProvider.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import { FC, ReactNode } from 'react';
-import {
-    QueryClient,
-    QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 interface IQueryProviderProps {
@@ -14,7 +11,6 @@ interface IQueryProviderProps {
 const QueryProvider: FC<IQueryProviderProps> = ({
     children,
 }: IQueryProviderProps) => {
-
     const queryClient = new QueryClient();
 
     return (
