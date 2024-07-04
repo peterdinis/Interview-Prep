d8550a6fff4cc5c3c368d3384cfb49bfdadbc1c8
Peter Dinis <peterdinis0@pobox.sk>
2024-07-04 17:04:23 +0200

    feat: Write endpoint for delete interview

--- app/api/interview/[id]/route.ts
+++ app/api/interview/[id]/route.ts
@@ -1,7 +1,7 @@
 import { db } from 'database/db';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
     const url = new URL(request.url);
     const id = url.pathname.split('/').pop();
 
@@ -27,6 +27,53 @@ export async function GET(request: Request) {
         return NextResponse.json(interview);
     } catch (error) {
         console.error('Error fetching interview:', error);
+        return NextResponse.json(
+            { error: 'Internal Server Error' },
+            { status: 500 },
+        );
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+    const url = new URL(request.url);
+    const id = url.pathname.split('/').pop();
+
+    if (!id) {
+        return NextResponse.json(
+            { error: 'Missing id parameter' },
+            { status: 400 },
+        );
+    }
+
+    try {
+        const interview = await db.interview.findFirst({
+            where: { id },
+        });
+
+        if (!interview) {
+            return NextResponse.json(
+                { error: 'Interview not found' },
+                { status: 404 },
+            );
+        }
+
+        const interviewToDelete = await db.interview.delete({
+            where: {
+                id: interview.id
+            }
+        })
+
+        if(!interviewToDelete) {
+            return NextResponse.json({
+                error: "Failed to delete interview",
+                status: 500
+            })
+        }
+
+        return {
+            message: "Interview was deleted"
+        }
+    } catch(error) {
         return NextResponse.json(
             { error: 'Internal Server Error' },
             { status: 500 },
