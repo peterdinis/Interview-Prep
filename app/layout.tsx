97784d566638fcded519737f4607462438a316db
Peter Dinis <peterdinis0@pobox.sk>
2024-07-09 17:07:55 +0200

    feat: Scroll to top provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 import './globals.css';
 import 'react-vertical-timeline-component/style.min.css';
 import Navbar from './_components/shared/Navbar';
@@ -8,8 +8,9 @@ import SassUiProvider from './_components/shared/providers/SassUiProvider';
 import LayoutWrapper from './_components/shared/LayoutWrapper';
 import SessionAppProvider from './_components/shared/providers/SessionProvider';
 import QueryProvider from './_components/shared/providers/QueryProvider';
+import ScrollToTop from './_components/shared/providers/ScrollToTopProvider';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Nunito({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Interview Prep',
@@ -30,6 +31,7 @@ export default function RootLayout({
                             <Navbar />
                             <LayoutWrapper>
                                 <QueryProvider>{children}</QueryProvider>
+                                <ScrollToTop />
                             </LayoutWrapper>
                         </SessionAppProvider>
                     </SassUiProvider>
