d5df00918ca66f003012202ee43f916d731f0129
Peter Dinis <peterdinis0@pobox.sk>
2024-07-11 16:08:02 +0200

    feat: Finish classc mode for application

--- app/api/interview/route.ts
+++ app/api/interview/route.ts
@@ -3,72 +3,77 @@ import { db } from 'database/db';
 
 export async function POST(req: NextRequest) {
     try {
-        const {
-            jobPosition,
-            jobDesc,
-            jobExperience,
-            numQuestions,
-            showQuestions,
-        } = await req.json();
+        const { jobPosition, jobDesc, jobExperience, numQuestions, showQuestions } =
+            await req.json();
 
-        if (
-            !jobPosition ||
-            !jobDesc ||
-            !jobExperience ||
-            (showQuestions && !numQuestions)
-        ) {
+        if (!jobPosition || !jobDesc || !jobExperience || (showQuestions && !numQuestions)) {
             return NextResponse.json(
                 { error: 'Missing required fields' },
                 { status: 400 },
             );
         }
 
+        const openaiResponse = await fetch(
+            'https://api.openai.com/v1/completions',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+                },
+                body: JSON.stringify({
+                    prompt: showQuestions 
+                        ? `Create a mock interview for a ${jobPosition} with experience in ${jobDesc} and ${jobExperience} years of experience. Generate ${numQuestions} questions.` 
+                        : `Create a mock interview for a ${jobPosition} with experience in ${jobDesc} and ${jobExperience} years of experience. Generate ${numQuestions} questions with detailed answers.`,
+                    model: 'gpt-3.5-turbo-instruct',
+                    max_tokens: 2048,
+                    n: 1,
+                    temperature: 0.7,
+                }),
+            },
+        );
+
+        if (!openaiResponse.ok) {
+            const errorText = await openaiResponse.text();
+            console.error('OpenAI API error:', errorText);
+            return NextResponse.json(
+                { error: 'Error generating mock interview from OpenAI API' },
+                { status: openaiResponse.status },
+            );
+        }
+
+        const data = await openaiResponse.json();
+
+        if (!data.choices || data.choices.length === 0) {
+            return NextResponse.json(
+                { error: 'No choices returned from OpenAI API' },
+                { status: 500 },
+            );
+        }
+
+        const generatedText = data.choices[0].text.trim();
         let questions = [];
+        let answers: any[] = [];
 
         if (showQuestions) {
-            const openaiResponse = await fetch(
-                'https://api.openai.com/v1/completions',
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-                    },
-                    body: JSON.stringify({
-                        prompt: `Create a mock interview for a ${jobPosition} with experience in ${jobDesc} and ${jobExperience} years of experience. Generate ${numQuestions} questions.`,
-                        model: 'gpt-3.5-turbo-instruct',
-                        max_tokens: 2048,
-                        n: 1,
-                        temperature: 0.7,
-                    }),
-                },
-            );
-
-            if (!openaiResponse.ok) {
-                const errorText = await openaiResponse.text();
-                console.error('OpenAI API error:', errorText);
-                return NextResponse.json(
-                    {
-                        error: 'Error generating mock interview from OpenAI API',
-                    },
-                    { status: openaiResponse.status },
-                );
-            }
-
-            const data = await openaiResponse.json();
-
-            if (!data.choices || data.choices.length === 0) {
-                return NextResponse.json(
-                    { error: 'No choices returned from OpenAI API' },
-                    { status: 500 },
-                );
-            }
-
-            const generatedText = data.choices[0].text.trim();
             questions = generatedText
                 .split('\n')
-                .filter((q: any) => q.trim().length > 0)
+                .filter((q: string) => q.trim().length > 0)
                 .slice(0, numQuestions); // Limit the number of questions to the requested number
+        } else {
+            const qnaPairs = generatedText
+                .split('\n\n')
+                .filter((qna: string) => qna.trim().length > 0)
+                .slice(0, numQuestions); // Limit the number of questions to the requested number
+
+            qnaPairs.forEach((pair: { split: (arg0: string) => [any, ...any[]]; }) => {
+                const [question, ...answerParts] = pair.split('\n');
+                const answer = answerParts.join(' ').trim();
+                if (question && answer) {
+                    questions.push(question.trim());
+                    answers.push(answer);
+                }
+            });
         }
 
         const newInterview = await db.interview.create({
@@ -76,13 +81,12 @@ export async function POST(req: NextRequest) {
                 jobPosition,
                 jobDesc,
                 jobExperience: jobExperience,
-                mockInterview: showQuestions ? questions.join('\n') : '',
+                mockInterview: generatedText,
                 questions: {
-                    create: showQuestions
-                        ? questions.map((question: string) => ({
-                              question,
-                          }))
-                        : [],
+                    create: questions.map((question: string, index: number) => ({
+                        question,
+                        answer: showQuestions ? '' : answers[index] || '',
+                    })),
                 },
             },
             include: {
