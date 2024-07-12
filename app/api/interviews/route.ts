60660f6f5604735e9735104edd95fb59295b2e60
Peter Dinis <peterdinis0@pobox.sk>
2024-07-12 10:53:52 +0200

    fix: Add where condition to api for displaying interviews

--- app/api/interviews/route.ts
+++ app/api/interviews/route.ts
@@ -1,10 +1,28 @@
+import { CustomSession } from 'app/_types/sessionTypes';
 import { db } from 'database/db';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
+import authOptions from '../auth/authOptions';
 
 export async function GET() {
-    const interviews = await db.interview.findMany();
+    const session = (await getServerSession(authOptions)) as CustomSession;
+
+    if (!session || !session.user) {
+        return NextResponse.json(
+            { error: 'Not authenticated' },
+            { status: 401 },
+        );
+    }
+
+    const userId = session.user.id;
+
+    const interviews = await db.interview.findMany({
+        where: {
+            userId,
+        },
+    });
     if (!interviews) {
-        throw new Error('No interviews found');
+        throw new Error('User does not create any interviews');
     }
 
     return NextResponse.json(interviews);
