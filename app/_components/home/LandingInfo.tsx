7cddde8d5a955391b04f3d6aa2debcdc0e8ac6b1
Peter Dinis <peterdinis0@pobox.sk>
2024-07-13 15:11:32 +0200

    feat: Update text in Landing info

--- app/_components/home/LandingInfo.tsx
+++ app/_components/home/LandingInfo.tsx
@@ -25,7 +25,11 @@ const milestones = [
         id: 2,
         date: 'Step 2',
         title: 'Create mock interview',
-        description: `Test your knowledge on a mock interview so that you can star in the real interview.`,
+        description: `Test your knowledge on a mock interview so that you can star in the real interview. You can choose between two modes. Classic & Questions Mode.
+        Classic mode -> AI generate all interview for you(Questions and answers).
+        Question mode -> AI generate question for you and you answer for this questions.
+
+        `,
     },
     {
         id: 3,
