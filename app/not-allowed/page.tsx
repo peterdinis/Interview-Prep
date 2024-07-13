502a3a0cc5f2a8ac33cd6f97556ebe6af3b04ffd
Peter Dinis <peterdinis0@pobox.sk>
2024-07-13 17:10:18 +0200

    FOO

--- app/not-allowed/page.tsx
+++ app/not-allowed/page.tsx
@@ -4,7 +4,7 @@ import NotAllowedWrapper from 'app/_components/shared/NotAllowedWrapper';
 import { NextPage } from 'next';
 
 const NotAllowedPage: NextPage = () => {
-    return <NotAllowedWrapper />
+    return <NotAllowedWrapper />;
 };
 
 export default NotAllowedPage;
