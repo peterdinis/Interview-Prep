94bfa0bd08813ec13b0fed508fb8c8585a045bca
Peter Dinis <peterdinis0@pobox.sk>
2024-07-13 15:46:40 +0200

    feat: Add validation to LoginForm

--- app/_components/auth/authSchemas.ts
+++ app/_components/auth/authSchemas.ts
@@ -4,10 +4,14 @@ export const registerSchema = z.object({
     name: z.string().min(1),
     email: z
         .string()
-        .email({ message: 'Invalid email format' })
-        .nonempty({ message: 'Email is required' }),
+        .email({ message: 'Invalid email format' }),
     password: z
         .string()
         .min(6, { message: 'Password must be at least 6 characters' })
-        .nonempty({ message: 'Password is required' }),
+});
+
+
+export const loginSchema = z.object({
+    email: z.string().email({ message: 'Invalid email format' }).nonempty({ message: 'Email is required' }),
+    password: z.string().min(1)
 });
