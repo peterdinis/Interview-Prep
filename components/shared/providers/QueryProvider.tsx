0a82228e5d7a372134f113293e38b7363abf8164
peterdinis <petter1@pobox.sk>
2025-06-08 16:34:44 +0200

    infra: Formatting files

--- components/shared/providers/QueryProvider.tsx
+++ components/shared/providers/QueryProvider.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { FC, ReactNode } from 'react';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { FC, ReactNode } from "react";
 
 type QueryProviderProps = {
-    children?: ReactNode;
+	children?: ReactNode;
 };
 
 const QueryProvider: FC<QueryProviderProps> = ({ children }) => {
-    const queryClient = new QueryClient();
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    );
+	const queryClient = new QueryClient();
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 };
 
 export default QueryProvider;
