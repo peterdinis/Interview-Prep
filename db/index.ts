031db1f61abe20632f19bc46fae0aa1764467f44
peterdinis <petter1@pobox.sk>
2025-06-14 14:48:54 +0200

    fix: Fixing issues WIP

--- db/index.ts
+++ db/index.ts
@@ -1,11 +1,6 @@
 import Database from "better-sqlite3";
-import {
-	type BetterSQLite3Database,
-	drizzle,
-} from "drizzle-orm/better-sqlite3";
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { drizzle, type BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 
+// ✅ Create the DB instance and export it
 const sqlite = new Database("sqlite.db");
 export const db: BetterSQLite3Database = drizzle(sqlite);
-
-migrate(db, { migrationsFolder: "drizzle" });
