12d89c6303286f99a76b0bc6438d66c677319ff6
peterdinis <petter1@pobox.sk>
2025-06-29 18:35:40 +0200

    feat: Migrate from sqlite to postgresql

--- db/index.ts
+++ db/index.ts
@@ -2,11 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-	host: "localhost",
-	port: 5432,
-	user: "your_username",
-	password: "your_password",
-	database: "your_database_name",
+	connectionString: process.env.DATABASE_URL!,
 });
 
 export const db = drizzle(pool);
