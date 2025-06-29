342590fbdfedc3721df3071325b6cc4644ba4ace
peterdinis <petter1@pobox.sk>
2025-06-29 18:32:57 +0200

    feat: Migrate from sqlite to postgresql

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,12 +3,8 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
 	out: "./drizzle",
 	schema: "./db/schema.ts",
-	dialect: "postgresql", // ✅ change from "sqlite" to "postgresql"
+	dialect: "postgresql",
 	dbCredentials: {
-		host: "localhost",
-		port: 5432,
-		user: "your_username",
-		password: "your_password",
-		database: "your_database_name",
+		url: process.env.DATABASE_URL!
 	},
 });
